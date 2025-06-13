d9324296e3f17ff05fcc2348f3df4445883f708b
JavaThugNGG <ildarjava@mail.ru>
2025-06-13 16:41:23 +0300

    test: add new tests for scoring logic

--- src/test/java/MatchScoreCalculationServiceTest.java
+++ src/test/java/MatchScoreCalculationServiceTest.java
@@ -5,75 +5,177 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MatchScoreCalculationServiceTest {
+
     @Test
-    void scoringFirstPlayerPointsWithoutAdvantageAtDeuceShouldNotEndGame() {
+    void ScoringFirstPlayerPointsWhenScore40_0ShouldUpdateFirstPlayerGamesFrom0To1() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerGames());
+    }
+
+    @Test
+    void ScoringFirstPlayerPointsWhenScore40_15ShouldUpdateFirstPlayerGamesFrom0To1() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,15);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerGames());
+    }
+
+    @Test
+    void ScoringFirstPlayerPointsWhenScore40_30ShouldUpdateFirstPlayerGamesFrom0To1() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,30);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerGames());
+    }
+
+    @Test
+    void ScoringFirstPlayerPointsWhenScore40_40WithoutAdvantagesShouldNotUpdateFirstPlayerGamesFrom0To1() {
         MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,40);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
         matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(1, matchScoreModel.getFirstPlayerAdvantage());
         assertEquals(0, matchScoreModel.getFirstPlayerGames());
     }
 
+    //тут дописываю
+
     @Test
-    void scoringFirstPlayerPointsWithOneAdvantageAtDeuceShouldNotEndGame() {
-        MatchScoreModel matchScoreModel = new MatchScoreModel(1, 2, 0, 0, 0, 0, 40, 40);
+    void ScoringFirstPlayerPointsWhenScore40_40WithOneAdvantagesShouldNotUpdateFirstPlayerGamesFrom0To1() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,40);
         matchScoreModel.setFirstPlayerAdvantage(1);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
         matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(2, matchScoreModel.getFirstPlayerAdvantage());
         assertEquals(0, matchScoreModel.getFirstPlayerGames());
     }
 
     @Test
-    void scoringFirstPlayerPointsWithTwoAdvantagesAtDeuceShouldEndGame() {
+    void ScoringFirstPlayerPointsWhenScore40_40WithTwoAdvantagesShouldUpdateFirstPlayerGames() {
         MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,40);
         matchScoreModel.setFirstPlayerAdvantage(2);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
         matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(0, matchScoreModel.getFirstPlayerAdvantage());
         assertEquals(1, matchScoreModel.getFirstPlayerGames());
     }
 
+
+
     @Test
-    void scoringSecondPlayerPointsWithoutAdvantageAtDeuceShouldNotEndGame() {
-        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,40);
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_0() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,0,40,0);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
-        matchScoreCalculationService.scoringSecondPlayer(matchScoreModel);
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(1, matchScoreModel.getSecondPlayerAdvantage());
-        assertEquals(0, matchScoreModel.getSecondPlayerGames());
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
     }
 
     @Test
-    void scoringSecondPlayerWithOneAdvantagePointsAtDeuceShouldNotEndGame() {
-        MatchScoreModel matchScoreModel = new MatchScoreModel(1, 2, 0, 0, 0, 0, 40, 40);
-        matchScoreModel.setSecondPlayerAdvantage(1);
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_1() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,1,40,0);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
-        matchScoreCalculationService.scoringSecondPlayer(matchScoreModel);
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(2, matchScoreModel.getSecondPlayerAdvantage());
-        assertEquals(0, matchScoreModel.getSecondPlayerGames());
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
     }
 
     @Test
-    void scoringSecondPlayerPointsWithTwoAdvantagesAtDeuceShouldEndGame() {
-        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,0,0,40,40);
-        matchScoreModel.setSecondPlayerAdvantage(2);
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_2() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,2,40,0);
 
         MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
-        matchScoreCalculationService.scoringSecondPlayer(matchScoreModel);
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
 
-        assertEquals(0, matchScoreModel.getSecondPlayerAdvantage());
-        assertEquals(1, matchScoreModel.getSecondPlayerGames());
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
     }
+
+    @Test
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_3() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,3,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
+    }
+
+    @Test
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_4() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,4,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
+    }
+
+    @Test
+    void ScoringFirstPlayerGamesShouldNotUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_5() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,5,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(0, matchScoreModel.getFirstPlayerSets());
+    }
+
+    @Test
+    void ScoringFirstPlayerGamesShouldNotUpdateFirstPlayerSetsFrom0To1WhileGamesScore6_6() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,5,6,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(0, matchScoreModel.getFirstPlayerSets());
+    }
+
+    @Test
+    void ScoringFirstPlayerGamesShouldUpdateFirstPlayerSetsFrom0To1WhileGamesScore7_6() {
+        MatchScoreModel matchScoreModel = new MatchScoreModel(1,2,0,0,6,6,40,0);
+
+        MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+        matchScoreCalculationService.scoringFirstPlayer(matchScoreModel);
+
+        assertEquals(1, matchScoreModel.getFirstPlayerSets());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
