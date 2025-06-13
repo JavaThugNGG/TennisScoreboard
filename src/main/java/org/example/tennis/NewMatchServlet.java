61b411e8097e6ae45f84b2001d881a122d2bb765
JavaThugNGG <ildarjava@mail.ru>
2025-06-13 21:51:25 +0300

    fix: Resolve issue with creating new matches after firs match

--- src/main/java/org/example/tennis/NewMatchServlet.java
+++ src/main/java/org/example/tennis/NewMatchServlet.java
@@ -91,8 +91,6 @@ public class NewMatchServlet extends HttpServlet {
 
         UUID pastedId = ongoingMatchesService.addMatch(matchScoreModel);
 
-        getServletContext().removeAttribute("currentMatches");
-
         getServletContext().setAttribute("ongoingMatchesService", ongoingMatchesService);
 
 
