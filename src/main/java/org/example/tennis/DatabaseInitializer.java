5e92fbd9e3e192732eaaadd5d120ee9ded5c4265
JavaThugNGG <ildarjava@mail.ru>
2025-06-14 19:36:18 +0300

    refactor: clean up code

--- src/main/java/org/example/tennis/DatabaseInitializer.java
+++ src/main/java/org/example/tennis/DatabaseInitializer.java
@@ -4,7 +4,6 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 import org.h2.tools.Server;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -19,58 +18,38 @@ public class DatabaseInitializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        createH2Console();
 
-        try {
-            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+        createSessionFactory();
+        System.out.println("SessionFactory открыт.");                                 //в лог
+        sce.getServletContext().setAttribute("sessionFactory", sessionFactory);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        try {
-            // Создаем SessionFactory — здесь Hibernate создаст таблицы
-            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-            sce.getServletContext().setAttribute("SessionFactory", sessionFactory);
-
-            // Открываем сессию и транзакцию для инициализации данных
-            try (Session session = sessionFactory.getCurrentSession()) {
-                session.beginTransaction();
-
-                // Добавляем стартовые данные (например, игроков и матч)
-                PlayerEntity firstPlayer = new PlayerEntity("First");
-                PlayerEntity secondPlayer = new PlayerEntity("Second");
-                MatchEntity firstMatch = new MatchEntity(firstPlayer, secondPlayer, firstPlayer);
-                firstPlayer.setWinner(firstMatch);
-
-                session.persist(firstPlayer);
-                session.persist(secondPlayer);
-                session.persist(firstMatch);
-
-                MatchEntity match = session.get(MatchEntity.class, firstMatch.getId());
-                System.out.println(match);
-
-
-                session.getTransaction().commit();
-
-
-            }
-
-            Map<UUID, MatchScoreModel> currentMatches = new ConcurrentHashMap<>();      //передали мапу
-            sce.getServletContext().setAttribute("currentMatches", currentMatches);
-
-
-            System.out.println("Инициализация таблиц и данных выполнена успешно.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Map<UUID, MatchScoreModel> currentMatches = new ConcurrentHashMap<>();
+        sce.getServletContext().setAttribute("currentMatches", currentMatches);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         if (sessionFactory != null && !sessionFactory.isClosed()) {
             sessionFactory.close();
-            System.out.println("SessionFactory закрыт.");
+            System.out.println("SessionFactory закрыт.");                             //в лог
+        }
+    }
+
+
+    private void createH2Console() {
+        try {
+            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
+        } catch (Exception e) {
+            e.printStackTrace();    //сделаешь в лог
+        }
+    }
+
+    private void createSessionFactory() {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
