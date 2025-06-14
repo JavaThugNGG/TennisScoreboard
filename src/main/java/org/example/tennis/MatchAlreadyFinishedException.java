078ed6a87724768d5c1dbac49d177760ae26103d
JavaThugNGG <ildarjava@mail.ru>
2025-06-14 20:51:17 +0300

    refactor(exception): remove redundant code
    to squash

--- src/main/java/org/example/tennis/MatchAlreadyFinishedException.java
+++ src/main/java/org/example/tennis/MatchAlreadyFinishedException.java
@@ -2,11 +2,7 @@ package org.example.tennis;
 
 public class MatchAlreadyFinishedException extends RuntimeException {
     public MatchAlreadyFinishedException() {
-        super("Match is already finished.");
-    }
-
-    public MatchAlreadyFinishedException(String message) {
-        super(message);
+        super();
     }
 }
 
