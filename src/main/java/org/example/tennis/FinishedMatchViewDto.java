99ed03004c373b6ba3c8d3d0f1c89f40f0a71c58
JavaThugNGG <ildarjava@mail.ru>
2025-07-17 14:05:47 +0300

    refactor(service): rename class name for better readability

--- src/main/java/org/example/tennis/FinishedMatchViewDto.java
+++ src/main/java/org/example/tennis/FinishedMatchViewDto.java
@@ -3,12 +3,12 @@ package org.example.tennis;
 import lombok.Getter;
 
 @Getter
-public class FinishedMatchDto {
+public class FinishedMatchViewDto {
     private final MatchScoreModel currentMatch;
     private final String firstPlayerResult;
     private final String secondPlayerResult;
 
-    FinishedMatchDto(MatchScoreModel currentMatch, PlayersResultDto playersResult) {
+    FinishedMatchViewDto(MatchScoreModel currentMatch, PlayersResultDto playersResult) {
         this.currentMatch = currentMatch;
         this.firstPlayerResult = String.valueOf(playersResult.getFirstPlayerResult());
         this.secondPlayerResult = String.valueOf(playersResult.getSecondPlayerResult());
