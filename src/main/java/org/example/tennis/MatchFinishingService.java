08cec4b5a35600b3fdcbd73b64fc37f4df35bdac
JavaThugNGG <ildarjava@mail.ru>
2025-07-17 14:05:16 +0300

    refactor(service): rename class and method names for better readability

--- src/main/java/org/example/tennis/MatchFinishingService.java
+++ src/main/java/org/example/tennis/MatchFinishingService.java
@@ -1,20 +1,49 @@
 package org.example.tennis;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import java.util.UUID;
-
 public class MatchFinishingService {
-    private final MatchEndingService matchEndingService;
 
-    public MatchFinishingService(MatchEndingService matchEndingService) {
-        this.matchEndingService = matchEndingService;
+    public PlayersResultDto persistMatch(SessionFactory sessionFactory, int firstPlayerId, int secondPlayerId, PlayerSide winner) {
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        try {
+            PlayerEntity firstPlayer = session.get(PlayerEntity.class, firstPlayerId);
+            PlayerEntity secondPlayer = session.get(PlayerEntity.class, secondPlayerId);
+
+            MatchEntity matchEntity = buildMatch(firstPlayer, secondPlayer, winner);
+
+            session.persist(matchEntity);
+            session.getTransaction().commit();
+
+            return determinePlayersResult(winner);
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        }
     }
 
-    public FinishedMatchDto handleFinishedMatch(MatchScoreModel currentMatch, PlayerSide winnerSide, SessionFactory sessionFactory, int firstPlayerId, int secondPlayerId, OngoingMatchesService ongoingMatchesService, UUID matchIUuid) {
-        PlayersResultDto playersResult = matchEndingService.persist(sessionFactory, firstPlayerId, secondPlayerId, winnerSide);
-        ongoingMatchesService.deleteMatch(matchIUuid);
-        return new FinishedMatchDto(currentMatch, playersResult);
+    private MatchEntity buildMatch(PlayerEntity firstPlayer, PlayerEntity secondPlayer, PlayerSide winner) {
+        MatchEntity matchEntity = new MatchEntity();
+        matchEntity.setPlayer1(firstPlayer);
+        matchEntity.setPlayer2(secondPlayer);
+
+        if (winner == PlayerSide.FIRST) {
+            matchEntity.setWinner(firstPlayer);
+        } else {
+            matchEntity.setWinner(secondPlayer);
+        }
+
+        return matchEntity;
+    }
+
+    private PlayersResultDto determinePlayersResult(PlayerSide winner) {
+        if (winner == PlayerSide.FIRST) {
+            return new PlayersResultDto(PlayerResult.WINNER, PlayerResult.LOSER);
+        } else {
+            return new PlayersResultDto(PlayerResult.LOSER, PlayerResult.WINNER);
+        }
     }
 }
 
