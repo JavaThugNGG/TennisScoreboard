3cbed0defbb9ce3ccba286f6d45f1c218ab8c485
JavaThugNGG <ildarjava@mail.ru>
2025-07-23 14:51:15 +0300

    refactor(servlet): rename references
    to squash

--- src/main/java/org/example/tennis/MatchesServlet.java
+++ src/main/java/org/example/tennis/MatchesServlet.java
@@ -12,26 +12,19 @@ import java.io.IOException;
 @WebServlet("/matches")
 public class MatchesServlet extends HttpServlet {
     private final SessionFactory sessionFactory = SessionFactoryManager.getInstance().getSessionFactory();
-    private final MatchPageViewService paginatedMatchViewService = new MatchPageViewService(sessionFactory);
+    private final MatchPageViewService matchPageViewService = new MatchPageViewService(sessionFactory);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String page = request.getParameter("page");
         String playerNameFilter = request.getParameter("filter_by_player_name");
 
-        MatchPageViewDto matchPage = paginatedMatchViewService.getPage(page, playerNameFilter);
+        MatchPageViewDto matchPage = matchPageViewService.getPage(page, playerNameFilter);
 
         request.setAttribute("matchPage", matchPage);
         request.getRequestDispatcher("/WEB-INF/matches.jsp").forward(request, response);
     }
 }
-
-
-
-
-
-
-
 
 
 
