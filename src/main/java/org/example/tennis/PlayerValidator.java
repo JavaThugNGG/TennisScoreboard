d1d610a805659f17031b37233892373df92dc773
JavaThugNGG <ildarjava@mail.ru>
2025-07-23 14:24:59 +0300

    fix(validator): limit player name to 16 chars to prevent UI overflow

--- src/main/java/org/example/tennis/PlayerValidator.java
+++ src/main/java/org/example/tennis/PlayerValidator.java
@@ -1,10 +1,17 @@
 package org.example.tennis;
 
 public class PlayerValidator {
+    private final static int MAX_PLAYER_NAME_LENGTH = 16;
 
-    public void validateId(String playerId) {
-        if (playerId == null || playerId.isBlank()) {
-            throw new IllegalArgumentException("Некорректное значение playerId: " + playerId);
+    public void validateName(String name) {
+        if (name == null || name.trim().isEmpty() || name.length() > MAX_PLAYER_NAME_LENGTH) {
+            throw new IllegalArgumentException("Некорректное имя игрока: " + name);
+        }
+    }
+
+    public void validateId(String id) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("Некорректное значение id игрока: " + id);
         }
     }
 }
