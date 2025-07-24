a4d408c6393d6f08d5eb5110eb91d205bd4b2516
JavaThugNGG <ildarjava@mail.ru>
2025-07-24 20:06:48 +0300

    chore(error-handling): remove unused exception cases in exception-to-status mapping logic

--- src/main/java/org/example/tennis/StatusCodeProcessor.java
+++ src/main/java/org/example/tennis/StatusCodeProcessor.java
@@ -7,9 +7,6 @@ public class StatusCodeProcessor {
     public int resolveStatusCode(Throwable throwable) {
         return switch (throwable.getClass().getSimpleName()) {
             case "IllegalArgumentException" -> HttpServletResponse.SC_BAD_REQUEST;
-            case "ElementNotFoundException" -> HttpServletResponse.SC_NOT_FOUND;
-            case "ElementAlreadyExistsException" -> HttpServletResponse.SC_CONFLICT;
-            case "DatabaseException" -> HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
             default -> HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
         };
     }
